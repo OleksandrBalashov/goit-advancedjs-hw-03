cdb56092e67f092744f5e54def661f99947b8d74
OleksandrBalashov <aleksandr.balashov.sax@gmai.com>
2023-11-19 11:09:24 +0100

    refactor

--- src/markups.js
+++ src/markups.js
@@ -2,13 +2,13 @@ function createCatInfoMarkup(data) {
   return data.breeds
     .map(
       ({ name, description, temperament }) => `
-    <img src="${data.url}" width="500" alt="${name}" class="img" />
-    <div>
-      <h2 class="title">${name}</h2>
-      <p class="desc">${description}</p>
-      <p class="desc"><b>Temperament: </b>${temperament}</p>
-    </div>
-  `
+        <img src="${data.url}" width="500" alt="${name}" class="img" />
+        <div>
+          <h2 class="title">${name}</h2>
+          <p class="desc">${description}</p>
+          <p class="desc"><b>Temperament: </b>${temperament}</p>
+        </div>
+      `
     )
     .join('');
 }
